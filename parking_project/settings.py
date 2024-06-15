e7a4c091d5c751be10187e5e80571fd87507e6ab
ShyamsaiPrasad <shyamsaiprasadsammeta@gmail.com>
2024-06-16 01:02:04 +0530

    settingscommit

--- parking_project/settings.py
+++ parking_project/settings.py
@@ -76,12 +76,9 @@ WSGI_APPLICATION = 'parking_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'parking',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
